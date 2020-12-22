22b99318f21d8e7a34bb187aaadbde4379a347fb
bokuweb <bokuweb12@gmail.com>
2020-12-22 20:41:21 +0900

    feat: Add letter

--- src/parser.rs
+++ src/parser.rs
@@ -1,4 +1,4 @@
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq)]
 pub enum ParserError {
     ParsePositionError,
     NotSatisfy,
@@ -10,18 +10,26 @@ pub trait Parser {
     fn parse<'a>(&self, input: &'a str) -> Result<(Self::Output, &'a str), ParserError>;
 }
 
-pub fn digit() -> impl Parser<Output = char> {
+pub fn digit() -> impl Parser<Output = String> {
     satisfy(|c| c.is_digit(10))
 }
 
-// pub fn letter<Input>() -> impl Parser<Input, Output = char, PartialState = ()>
-// where
-//     Input: Stream<Token = char>,
-//     Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
-// {
-//     satisfy(|ch: char| ch.is_alphabetic()).expected("letter")
-// }
-//
+pub fn upper() -> impl Parser<Output = String> {
+    satisfy(|c| c.is_uppercase())
+}
+
+pub fn lower() -> impl Parser<Output = String> {
+    satisfy(|c| c.is_lowercase())
+}
+
+pub fn alphanumeric() -> impl Parser<Output = String> {
+    satisfy(|c| c.is_alphanumeric())
+}
+
+pub fn letter() -> impl Parser<Output = String> {
+    satisfy(|c| c.is_alphabetic())
+}
+
 pub fn satisfy<F>(predicate: F) -> Satisfy<F>
 where
     F: Fn(char) -> bool,
@@ -29,23 +37,13 @@ where
     Satisfy { predicate }
 }
 
-// fn satisfy<F>(test: F) -> Result<char, ParserError>
-// where
-//     F: Fn(char) -> bool,
-// {
-//     if !test(self.peak()?) {
-//         return Err(ParserError::NotSatisfy);
-//     }
-//     self.consume_char()
-// }
-
 #[derive(Copy, Clone)]
 pub struct Satisfy<F: Fn(char) -> bool> {
     predicate: F,
 }
 
 impl<F: Fn(char) -> bool> Parser for Satisfy<F> {
-    type Output = char;
+    type Output = String;
 
     fn parse<'a>(&self, input: &'a str) -> Result<(Self::Output, &'a str), ParserError> {
         let mut iter = input.char_indices();
@@ -56,16 +54,16 @@ impl<F: Fn(char) -> bool> Parser for Satisfy<F> {
         }
         let (next_pos, _) = iter.next().unwrap_or((1, ' '));
         // self.pos += next_pos;
-        Ok((cur_char, &input[next_pos..]))
+        Ok((cur_char.to_string(), &input[next_pos..]))
     }
 }
 
 #[derive(Copy, Clone)]
-pub struct Many<P: Parser<Output = char>> {
+pub struct Many<P: Parser<Output = String>> {
     parser: P,
 }
 
-impl<P: Parser<Output = char>> Parser for Many<P> {
+impl<P: Parser<Output = String>> Parser for Many<P> {
     type Output = String;
 
     fn parse<'a>(&self, input: &'a str) -> Result<(Self::Output, &'a str), ParserError> {
@@ -75,7 +73,7 @@ impl<P: Parser<Output = char>> Parser for Many<P> {
             match self.parser.parse(input) {
                 Ok(c) => {
                     input = c.1;
-                    res.push(c.0);
+                    res = res + &c.0;
                 }
                 Err(_) => return Ok((res, input)),
             }
@@ -83,82 +81,60 @@ impl<P: Parser<Output = char>> Parser for Many<P> {
     }
 }
 
-fn many<'a>(parser: impl Parser<Output = char>) -> impl Parser<Output = String> {
+fn many<'a>(parser: impl Parser<Output = String>) -> impl Parser<Output = String> {
     Many { parser }
 }
 
-// pub fn many1<F, Input, P>(p: P) -> Many1<F, P>
-// where
-//     Input: Stream,
-//     F: Extend<P::Output> + Default,
-//     P: Parser<Input>,
-// {
-//     Many1(p, PhantomData)
-// }
-
-/*
 #[derive(Copy, Clone)]
-pub struct Many1<F, P>(P, PhantomData<fn() -> F>);
-impl<F, Input, P> Parser<Input> for Many1<F, P>
-where
-    Input: Stream,
-    F: Extend<P::Output> + Default,
-    P: Parser<Input>,
-{
-    type Output = F;
-    type PartialState = (bool, bool, F, P::PartialState);
-
-    parse_mode!(Input);
-    #[inline]
-    fn parse_mode_impl<M>(
-        &mut self,
-        mut mode: M,
-        input: &mut Input,
-        state: &mut Self::PartialState,
-    ) -> ParseResult<F, Input::Error>
-    where
-        M: ParseMode,
-    {
-        let (ref mut parsed_one, ref mut committed_state, ref mut elements, ref mut child_state) =
-            *state;
-
-        if mode.is_first() || !*parsed_one {
-            debug_assert!(!*parsed_one);
-
-            let (first, committed) = ctry!(self.0.parse_mode(mode, input, child_state));
-            elements.extend(Some(first));
-            // TODO Should PeekOk be an error?
-            *committed_state = !committed.is_peek();
-            *parsed_one = true;
-            mode.set_first();
-        }
-
-        let mut iter = Iter {
-            parser: &mut self.0,
-            committed: *committed_state,
-            input,
-            state: State::Ok,
-            partial_state: child_state,
-            mode,
-        };
-        elements.extend(iter.by_ref());
-
-        iter.into_result_fast(elements).map(|x| {
-            *parsed_one = false;
-            x
-        })
-    }
-
-    fn add_committed_expected_error(&mut self, errors: &mut Tracked<<Input as StreamOnce>::Error>) {
-        self.add_error(errors);
-    }
-
-    forward_parser!(Input, add_error parser_count, 0);
+pub struct Or<O, P1: Parser<Output = O>, P2: Parser<Output = O>> {
+    p1: P1,
+    p2: P2,
 }
-*/
+
+impl<O, P1: Parser<Output = O>, P2: Parser<Output = O>> Parser for Or<O, P1, P2> {
+    type Output = O;
+
+    fn parse<'a>(&self, input: &'a str) -> Result<(Self::Output, &'a str), ParserError> {
+        if let Ok(res) = self.p1.parse(input) {
+            return Ok(res);
+        }
+        self.p2.parse(input)
+    }
+}
+
+fn or<'a, O, P1: Parser<Output = O>, P2: Parser<Output = O>>(p1: P1, p2: P2) -> Or<O, P1, P2> {
+    Or { p1, p2 }
+}
 
 #[test]
-pub fn test() {
-    assert_eq!(digit().parse("123").unwrap(), ('1', "23"));
+pub fn test_digit() {
+    assert_eq!(digit().parse("123").unwrap(), ("1".to_string(), "23"));
+}
+
+#[test]
+pub fn test_letter() {
+    assert_eq!(letter().parse("abc").unwrap(), ("a".to_string(), "bc"));
+}
+
+#[test]
+pub fn test_digit_many() {
     assert_eq!(many(digit()).parse("123").unwrap(), ("123".to_owned(), ""));
+}
+
+#[test]
+pub fn test_or() {
+    assert_eq!(or(letter(), digit()).parse("a").unwrap(), ("a".to_string(), ""));
+    assert_eq!(or(letter(), digit()).parse("1").unwrap(), ("1".to_string(), ""));
+    assert_eq!(
+        or(letter(), digit()).parse("!"),
+        Err(ParserError::NotSatisfy)
+    );
+}
+
+#[test]
+pub fn test_or_many() {
+    assert_eq!(
+        many(or(letter(), digit())).parse("abc123").unwrap(),
+        ("abc123".to_string(), "")
+    );
 }
